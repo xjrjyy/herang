7325b40d48ea8ebbf2a5d57d544e52a748971641
jrjyy <1479412322@qq.com>
2023-03-14 15:50:17 +0800

    fix

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,34 @@ use std::{io::Write, error::Error};
 
 extern crate clap;
 
-use clap::App;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author = "jrjyy", version = "0.4.0", about = "Cyber Language", long_about = None)]
+struct Args {
+   #[arg(short, long)]
+   path: Option<String>,
+
+   #[arg(short, long)]
+   cpp: bool,
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let matches = App::new("HeRang")
-        .version("0.4.0")
-        .author("jrjyy")
-        .about("Cyber Language")
-        .args_from_usage("-p, --path=[FILE] 'Target file you want to execute'")
-        .get_matches();
+    let args = Args::parse();
 
     let mut env = herang::HeEnv::new();
     herang::init_env(&mut env)?;
-    if let Some(source_file) = matches.value_of("path") {
-        let source = std::fs::read_to_string(source_file)?;
-        let result = herang::gen_code(&source, &mut env);
-        println!("{}", match result {
-            Ok(code) => code,
-            Err(msg) => msg,
-        });
-        // herang::eval(&source, &mut env)?;
+    if let Some(path) = args.path.as_deref() {
+        let source = std::fs::read_to_string(path)?;
+        if args.cpp {
+            let result = herang::gen_code(&source, &mut env);
+            println!("{}", match result {
+                Ok(code) => code,
+                Err(msg) => msg,
+            });
+        } else {
+            herang::eval(&source, &mut env)?;
+        }
     } else {
         loop {
             print!("herang> ");
